e50fb2551af507e985f46ca03463cf3db363a231
천명현 <cmh1448@naver.com>
2026-01-07 12:51:41 +0900

    Initial Commit

---
+++ src/main/java/depth/finvibe/finvibeuser/FinvibeUserApplication.java
@@ -0,0 +1,13 @@
+package depth.finvibe.finvibeuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinvibeUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinvibeUserApplication.class, args);
+    }
+
+}
