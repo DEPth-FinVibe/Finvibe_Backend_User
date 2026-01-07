9f752b493e54e1423c5e5fdce845058390df2561
천명현 <cmh1448@naver.com>
2026-01-07 13:04:48 +0900

    refactor: 패키지명 변경

--- src/main/java/depth/finvibe/user/boot/FinvibeUserApplication.java
+++ src/main/java/depth/finvibe/user/boot/FinvibeUserApplication.java
@@ -1,4 +1,4 @@
-package depth.finvibe.user;
+package depth.finvibe.user.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
