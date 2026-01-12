99545638264b130a1ba3a04a0ca8f00c7854d894
천명현 <cmh1448@naver.com>
2026-01-12 18:07:43 +0900

    fix: OAuth 설정에서 birthday와 mobile 스코프 제거 (구글 로그인 안되는 오류 수정)
    - application-oauth.yml에서 birthday와 mobile 스코프 삭제
    - FinvibeUserApplication.java에 JPA 감사 기능 활성화 추가

--- src/main/java/depth/finvibe/user/FinvibeUserApplication.java
+++ src/main/java/depth/finvibe/user/FinvibeUserApplication.java
@@ -2,8 +2,10 @@ package depth.finvibe.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class FinvibeUserApplication {
 
     public static void main(String[] args) {
